8db3dcd2575ba5a79f94c60788c369494edbec77
lufercho <luisfernandogilgareca@gmail.com>
2022-02-24 12:32:06 -0400

    3. Seleccionar estado y mostrar elección

--- src/presenter.js
+++ src/presenter.js
@@ -1,22 +1,16 @@
 const cantidad = document.querySelector("#cantidad_items");
+const precio = document.querySelector("#precio_items");
+const estado = document.querySelector("#estados");
+
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
-const precio = document.querySelector("#precio_items");
-const prec = document.querySelector("#precio-form");
-const resp = document.querySelector("#resultado-prec");
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   const firstNumber = Number.parseInt(cantidad.value);
- 
-  div.innerHTML = "<p> Cantidad de Item: " + firstNumber + "</p>";
-});
-
-prec.addEventListener("submit", (event) => {
-  event.preventDefault();
-
   const Precio1 = Number.parseInt(precio.value);
-  resp.innerHTML = "<p> Precio por Item: " + Precio1 + "</p>";
+  const estado1 = estado.value;
+  div.innerHTML = "<p> Cantidad de Item: " + firstNumber + "</p>" + "<p> Precio por Item: " + Precio1 + "</p>" + "<p> Estado: " + estado1 + "</p>";
 });
+
+
